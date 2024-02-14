07a04560ee0d0137d717adbace81a6fd70a40f9b
flngsquirrl <y.charniak@gmail.com>
2024-02-14 22:17:51 +0100

    Add scrum history

--- Scrumdinger/Models/DailyScrum.swift
+++ Scrumdinger/Models/DailyScrum.swift
@@ -21,6 +21,7 @@ struct DailyScrum: Identifiable {
         }
     }
     var theme: Theme
+    var history: [History] = []
 
     init(title: String, attendees: [String], lengthInMinutes: Int, theme: Theme) {
         self.title = title
